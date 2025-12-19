f6bf0aeb7ea888734d6d27de55714b35d683ca57
BOSSBATTLE <umer.scmanalyst@gmail.com>
2025-12-19 20:30:31 +0500

    the bat cave

---
+++ and or operators .py
@@ -0,0 +1,14 @@
+a=10
+b=-10
+c=0
+if a>0 or b>0:
+    print("condition is true")
+else:
+    print("condition is false")
+if a>0 or b>0:
+    print("condition is true")
+x=5
+y=10
+z=10
+print(x!=y)
+print(y!=z)
---
+++ temprature checking inquiery.py
@@ -0,0 +1,5 @@
+temp=int(input("enter the temperature where you are or nowadays"))
+if temp>25:
+    print("you can wear light clothes its burning hot")
+else:
+    print("no wear thick clothes its freezing out there")
