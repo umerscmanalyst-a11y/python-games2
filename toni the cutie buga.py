db2152ee4be186eaa90aea66ccf2ebd7b9e58cdd
BOSSBATTLE <umer.scmanalyst@gmail.com>
2026-01-10 18:12:26 +0500

    zoni the indian bebe

---
+++ toni the cutie buga.py
@@ -0,0 +1,6 @@
+katna=any(input(("peez enter yourr cutie buga age")))
+hugie=input("is yourr age beetween 20 wa 10 yes ow no")
+if hugie=="yes":
+    print("you shood pas bebe")
+else:
+    print("yoo shoodnt pas")
